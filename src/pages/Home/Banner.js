67f5aa77e2a7d9222cae669945aff9809f821752
Monirul-98 <monirulsifat@gmail.com>
2022-05-23 09:47:53 +0600

    Project setup

---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1618764400608-9e7115eede74?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
